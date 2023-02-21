446a243954a423711bb485c73771602734b3e8a4
Geoffrey Kong <79559478+CYBRXT@users.noreply.github.com>
2023-02-21 15:00:47 -0500

    Initial Commit

---
+++ KeyboardToolkit/KeyboardToolkitApp.swift
@@ -0,0 +1,17 @@
+//
+//  KeyboardToolkitApp.swift
+//  KeyboardToolkit
+//
+//  Created by Geoffrey Kong on 2023-02-21.
+//
+
+import SwiftUI
+
+@main
+struct KeyboardToolkitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
